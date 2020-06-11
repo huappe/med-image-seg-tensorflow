d33cf3e9b443dfde7d2882b1c63874e729e79ac1
huappe <155170805+huappe@users.noreply.github.com>
2020-06-11 07:27:06 +0000

    fix ignores

--- g_model.py
+++ g_model.py
@@ -17,3 +17,14 @@ from six.moves import xrange
 from utils import *
 from loss_functions import *
 from scipy.misc import imsave
+import collections
+import datetime
+
+import Queue, threading
+
+class seg_GAN(object):
+    def __init__(self, sess, batch_size=10, height=512,width=512, wd=0.0005, checkpoint_dir=None, path_patients_h5=None, learning_rate=2e-8,lr_step=30000,
+                 lam_dice=1, lam_fcn=1, lam_adv=1,adversarial=False):
+
+
+        self.sess = sess
