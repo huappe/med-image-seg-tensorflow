1167153c9541670df1d090cb915a29b78bfbdabc
huappe <155170805+huappe@users.noreply.github.com>
2020-08-29 16:18:31 +0000

    proper maintenance target

--- generate_2d_h5.py
+++ generate_2d_h5.py
@@ -6,3 +6,12 @@ Created on Jul 1, 2016
 '''
 import h5py, os
 import numpy as np
+import SimpleITK as sitk
+import scipy.io as scio
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+#import caffe
+from multiprocessing import Pool
+import argparse
+
+def worker(idx,namepatient,path_patients,dirname):
