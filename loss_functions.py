b32cf59036d6b6a6d3020d759422dc36994c5816
huappe <155170805+huappe@users.noreply.github.com>
2020-12-07 22:20:32 +0000

    Small refactorings

--- loss_functions.py
+++ loss_functions.py
@@ -6,4 +6,4 @@ import numpy as np
 def loss_dice(logits, labels, num_classes,batch_size_tf):
     """Calculate the loss from the logits and the labels.
     Args:
-      logits: tensor, 
+      logits: tensor, float - [ba
