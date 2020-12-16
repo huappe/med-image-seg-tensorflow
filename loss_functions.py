fbdc83ac7610cbf18f4c667ea3027999cf8c773f
huappe <155170805+huappe@users.noreply.github.com>
2020-12-16 19:19:15 +0000

    Added translation.

--- loss_functions.py
+++ loss_functions.py
@@ -6,4 +6,4 @@ import numpy as np
 def loss_dice(logits, labels, num_classes,batch_size_tf):
     """Calculate the loss from the logits and the labels.
     Args:
-      logits: tensor, float - [batch_size, width, height, num_
+      logits: tensor, float - [batch_size, width, height, num_classes
