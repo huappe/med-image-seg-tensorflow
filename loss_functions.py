2a319c5f72b91541ad9b116d6e06e41e1b2fb616
huappe <155170805+huappe@users.noreply.github.com>
2020-12-25 07:48:31 +0000

    new alias

--- loss_functions.py
+++ loss_functions.py
@@ -7,4 +7,4 @@ def loss_dice(logits, labels, num_classes,batch_size_tf):
     """Calculate the loss from the logits and the labels.
     Args:
       logits: tensor, float - [batch_size, width, height, num_classes].
-          Use vgg
+          Use vgg_fcn.up as logi
