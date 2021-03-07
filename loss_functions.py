7a7293bd0126ede0a212f525743a6bfd5b2b550b
huappe <155170805+huappe@users.noreply.github.com>
2021-03-07 13:30:22 +0000

    Added TravisCI badge

--- loss_functions.py
+++ loss_functions.py
@@ -13,3 +13,4 @@ def loss_dice(logits, labels, num_classes,batch_size_tf):
       weights: numpy array - [num_classes]
           Weighting the loss of each class
           Optional: Prioritize some classes
+    Returns:
