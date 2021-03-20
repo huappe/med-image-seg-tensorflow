63b4f845980ca7c4195950ae9a071d226e25167c
huappe <155170805+huappe@users.noreply.github.com>
2021-03-20 22:06:46 +0000

    Update et.po

--- loss_functions.py
+++ loss_functions.py
@@ -20,4 +20,4 @@ def loss_dice(logits, labels, num_classes,batch_size_tf):
     with tf.name_scope('loss'):
         #shapelables=labels.get_shape().as_list()
         probs=tf.nn.softmax(logits)        
-        y_onehot=tf.one_hot(labels,nu
+        y_onehot=tf.one_hot(labels,num_classes,1.
