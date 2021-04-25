8c4bddcb99e615cda65375f62003e2c2ff3f752c
huappe <155170805+huappe@users.noreply.github.com>
2021-04-25 22:01:07 +0000

    netvsc: don't flush peers notifying work during setting mtu

--- loss_functions.py
+++ loss_functions.py
@@ -33,4 +33,6 @@ def loss_dice(logits, labels, num_classes,batch_size_tf):
         #tf.add_to_collection('losses', dice_total)
         loss=dice_total
         #loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
-    return los
+    return loss
+
+def los
