4dbaada3bede8da3403370a0654b0d9ea4741ebc
huappe <155170805+huappe@users.noreply.github.com>
2021-05-15 20:41:54 +0000

    add property

--- loss_functions.py
+++ loss_functions.py
@@ -40,4 +40,4 @@ def lossfcn(logits, labels, num_classes,batch_size_tf,weights=None):
     Args:
       logits: tensor, float - [batch_size, width, height, num_classes].
           Use vgg_fcn.up as logits.
-      labels: Labels tensor, int32 - [batch_size, width, heig
+      labels: Labels tensor, int32 - [batch_size, width, height, num_classes
