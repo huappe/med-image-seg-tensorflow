2bd4679e2ebe8fe7ba1f78920b3981ba8a8c7165
huappe <155170805+huappe@users.noreply.github.com>
2021-05-23 12:57:16 +0000

    missing files

--- loss_functions.py
+++ loss_functions.py
@@ -42,4 +42,4 @@ def lossfcn(logits, labels, num_classes,batch_size_tf,weights=None):
           Use vgg_fcn.up as logits.
       labels: Labels tensor, int32 - [batch_size, width, height, num_classes].
           The ground truth of your data.
-      weights: numpy arr
+      weights: numpy array - [num_c
