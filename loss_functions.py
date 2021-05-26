18e4aebf6653ffa81afc9d27314c7dd994ba28f3
huappe <155170805+huappe@users.noreply.github.com>
2021-05-26 20:49:29 +0000

    gradle upgrade

--- loss_functions.py
+++ loss_functions.py
@@ -44,4 +44,4 @@ def lossfcn(logits, labels, num_classes,batch_size_tf,weights=None):
           The ground truth of your data.
       weights: numpy array - [num_classes]
           Weighting the loss of each class
-          Optional: Pr
+          Optional: Prioritize some class
