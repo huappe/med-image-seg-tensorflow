21f352aab0e23370835b443e9f307e5d47822186
huappe <155170805+huappe@users.noreply.github.com>
2021-08-11 08:16:06 +0000

    Development version

--- loss_functions.py
+++ loss_functions.py
@@ -66,3 +66,4 @@ def lossfcn(logits, labels, num_classes,batch_size_tf,weights=None):
             cross_entropy =labelweights*tf.nn.softmax_cross_entropy_with_logits(logits, labelsonehot, name=None)
             
         else:
+           
