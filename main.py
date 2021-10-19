c61f7a7eeeaad86f7683101e56de2ee9bb5c91f4
huappe <155170805+huappe@users.noreply.github.com>
2021-10-19 20:53:33 +0000

    sdcardfs: remove unneeded __init and __exit

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ pp = pprint.PrettyPrinter()
 flags = tf.app.flags
 flags.DEFINE_boolean("is_train", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("adverarial", False, "Adversarial or normal [50000]")
-flags.DEFINE_integer("iterations", 500000, "Epoch to tr
+flags.DEFINE_integer("iterations", 500000, "Epoch to train [50000]")
