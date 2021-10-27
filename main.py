142013ab1e45f6c01b596ad04e5b3793f2594d06
huappe <155170805+huappe@users.noreply.github.com>
2021-10-27 07:15:56 +0000

    Updated language

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ flags = tf.app.flags
 flags.DEFINE_boolean("is_train", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("adverarial", False, "Adversarial or normal [50000]")
 flags.DEFINE_integer("iterations", 500000, "Epoch to train [50000]")
+flags.DEFINE_fl
