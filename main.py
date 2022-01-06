e08059399b17e742c62dfbb131844a1e8f71ee3a
huappe <155170805+huappe@users.noreply.github.com>
2022-01-06 11:21:05 +0000

    here too

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ flags.DEFINE_float("learning_rate", 1e-8, "Learning rate of for SGD [1e-8]")
 flags.DEFINE_integer("batch_size", 10, "The size of batch images [10]")
 flags.DEFINE_integer("show_every", 10, "The size of batch images [10]")
 flags.DEFINE_integer("save_every", 1000, "save every [1000]")
-flags.DEFI
+flags.DEFINE_integer("test_eve
