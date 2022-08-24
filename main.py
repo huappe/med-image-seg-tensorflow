c382ac581e2e01c4065d33d6f8242a49d46ca9ce
huappe <155170805+huappe@users.noreply.github.com>
2022-08-24 10:38:26 +0000

    Prepare 0.0.3 release

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ flags.DEFINE_integer("batch_size", 10, "The size of batch images [10]")
 flags.DEFINE_integer("show_every", 10, "The size of batch images [10]")
 flags.DEFINE_integer("save_every", 1000, "save every [1000]")
 flags.DEFINE_integer("test_every", 2000, "test every [5000] iterations the subject")
-flags.DEFINE_integer("lr_step", 30000, "The step to decrease lr [lr_st
+flags.DEFINE_integer("lr_step", 30000, "The step to decrease lr [lr_step]")
+flags
