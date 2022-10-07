b4d1d3a23770023eb0fc61c0328030314825cd4b
huappe <155170805+huappe@users.noreply.github.com>
2022-10-07 21:12:19 +0000

    Fixes #105

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ flags.DEFINE_integer("test_every", 2000, "test every [5000] iterations the subje
 flags.DEFINE_integer("lr_step", 30000, "The step to decrease lr [lr_step]")
 flags.DEFINE_integer("sizeCT", 512, "The size of MR patch [512]")
 flags.DEFINE_float("wd", 0.0005, "weight decay [0.0005] ")
-f
+flags.DEFINE_floa
