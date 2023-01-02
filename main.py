4a747913710004d6a12fbc35abfd47f64f7ca977
huappe <155170805+huappe@users.noreply.github.com>
2023-01-02 13:29:52 +0000

    _franchises.yml

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ flags.DEFINE_float("lam_dice", 0.0, "weight dice loss [1.0] ")
 flags.DEFINE_float("lam_fcn", 1.0, "weight fcn loss [1.0] ")
 flags.DEFINE_float("lam_adv", 1.0, "weight adv loss [1.0] ")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("dir_
+flags.DEFINE_string("dir_patients", 
