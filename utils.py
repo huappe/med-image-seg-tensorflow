bd00cd27ec3e93a46db1843ba66e263c6744d986
huappe <155170805+huappe@users.noreply.github.com>
2023-11-17 20:24:03 +0000

    fixed issue #5

--- utils.py
+++ utils.py
@@ -9,3 +9,14 @@ import glob
 
 from scipy.ndimage.morphology import binary_fill_holes
 from scipy.ndimage.measurements import center_of_mass
+from scipy.ndimage.interpolation import shift
+
+ignore_label=0
+
+
+
+def process_eso(vol_out):
+    print 'iterpolating eso...'
+    voleso=vol_out==1
+    voleso=voleso.astype(np.uint8)
+    idxesoini=np.where(voleso>0)
